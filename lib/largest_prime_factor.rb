7bba12cbae96e482403d8771392555fa529e2f19
tejbans <singh.tejbans@gmail.com>
2015-11-17 14:36:48 -0800

    Done.

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,29 @@
-# Enter your procedural solution here!
+def largest_prime_factor(number)
+   x=2
+  while !prime(number)
+       if prime(x) && number % x == 0 
+           number= number/x
+           x+=1
+        else
+           x+=1
+        end
+  end
+number
+end
+
+def prime(num)
+  prime=true
+  counter=2
+  while counter<=num-1
+     if num%counter==0
+        prime=false
+        break
+      else
+        counter+=1
+      end
+  end
+prime
+end
+
+
+
--- lib/oo_largest_prime_factor.rb
+++ lib/oo_largest_prime_factor.rb
@@ -1,1 +1,34 @@
-# Enter your object-oriented solution here!
+class LargestPrimeFactor
+     
+def initialize(input)
+  @num=input
+end
+
+def number
+   x=2
+  while !prime(@num)
+       if prime(x) && @num % x == 0 
+           @num= @num/x
+           x+=1
+        else
+           x+=1
+        end
+  end
+  @num
+end
+
+def prime(num)
+  prime=true
+  counter=2
+  while counter<=num-1
+     if num%counter==0
+        prime=false
+        break
+      else
+        counter+=1
+      end
+  end
+  prime
+end
+
+end
